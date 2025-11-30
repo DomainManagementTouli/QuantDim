d2ce81ef0b0f53f9141fcea77240131cd93fb24b
DomainManagementTouli <megaquicktouli@gmail.com>
2025-11-30 06:21:41 -0600

    Delete auth-guard.js

--- auth-guard.js
+++
@@ -1,7 +0,0 @@
-// Add this script to any page you want to protect
-document.addEventListener('DOMContentLoaded', function() {
-    if (!Auth.isAuthenticated()) {
-        const currentPath = window.location.pathname.split('/').pop();
-        window.location.href = 'login.html?redirect=' + encodeURIComponent(currentPath);
-    }
-});
